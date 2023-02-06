72b2e04532136a597461e33108d5cdcbc4e97d20
Niclas Rosenvik <youremailsarecrap@gmail.com>
2023-02-06 20:29:59 +0000

    add STDC_ENDIAN defines

--- include/stdbit.h
+++ include/stdbit.h
@@ -1,4 +1,10 @@
 #ifndef _STDBIT_H_
 #define _STDBIT_H_
 
+#include <sys/endian.h>
+
+#define __STDC_ENDIAN_LITTLE__	_LITTLE_ENDIAN
+#define __STDC_ENDIAN_BIG__	_BIG_ENDIAN
+#define __STDC_ENDIAN_NATIVE__	_BYTE_ORDER
+
 #endif
