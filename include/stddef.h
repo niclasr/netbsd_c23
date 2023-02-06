5b02bc2a8a85e8f487658f3b6586b3306c33b639
Niclas Rosenvik <youremailsarecrap@gmail.com>
2023-02-06 20:37:03 +0000

    Add *_DECLS around declarations

--- include/stddef.h
+++ include/stddef.h
@@ -4,10 +4,14 @@
 
 #include_next <stddef.h>
 
+#include <sys/cdefs.h>
+
 #define __STDC_VERSION_STDDEF_H__ 202311L
 
 #define unreachable() nbunreachable()
 
+__BEGIN_DECLS
 void nbunreachable(void);
+__END_DECLS
 
 #endif
--- include/stdlib.h
+++ include/stdlib.h
@@ -4,10 +4,14 @@
 
 #include_next <stdlib.h>
 
+#include <sys/cdefs.h>
+
 #define __STDC_VERSION_STDLIB_H__ 202311L
 
+__BEGIN_DECLS
 void free_sized(void *, size_t);
 void free_aligned_sized(void *, size_t, size_t);
 size_t memalignment(const void *);
+__END_DECLS
 
 #endif
