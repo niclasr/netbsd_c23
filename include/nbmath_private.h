53ce136a2648ffe4d8c32b88fc67e839fedcd5fc
Niclas Rosenvik <youremailsarecrap@gmail.com>
2023-04-17 18:41:23 +0200

    Add copyright headers now that this repo is public

--- include/nbmath_private.h
+++ include/nbmath_private.h
@@ -1,3 +1,15 @@
+/*
+ * ====================================================
+ * Copyright (C) 1993 by Sun Microsystems, Inc. All rights reserved.
+ *
+ * Developed at SunPro, a Sun Microsystems, Inc. business.
+ * Permission to use, copy, modify, and distribute this
+ * software is freely granted, provided that this notice
+ * is preserved.
+ * ====================================================
+ */
+
+
 #ifndef _NBMATH_PRIVATE_H_
 #define _NBMATH_PRIVATE_H_
 
